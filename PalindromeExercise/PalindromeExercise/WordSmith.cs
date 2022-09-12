323b54f93d5801b0d23014e218fa03cc3ccdd754
Colby Pate <cbpate08@gmail.com>
2022-09-12 12:59:50 -0500

    Initial Commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //var wordReversed = new string(word.ToLower().Reverse().ToArray());
+            //return word.ToLower() == wordReversed ? true : false;
+            return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+
+        }
+    }
+
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineDataAttribute("racecar", true)]
+        [InlineDataAttribute("kayak", true)]
+        [InlineDataAttribute("Racecar", true)]
+        [InlineDataAttribute("Cactus", false)]
+        [InlineDataAttribute("programming", false)]
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var tester = new WordSmith();
 
+            //Act
+            var actual = tester.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
